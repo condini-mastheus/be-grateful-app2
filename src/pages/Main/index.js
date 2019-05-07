52ff4a1a2dfa2eabefafc5b574c0c692706bdfe7
Matheus Condini <matheus.condini@gmail.com>
2019-05-07 09:03:34 -0300

    Transform Main in functions to use react hooks

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,31 +4,35 @@ import { Container, Title, Content } from './styles';
 
 import PostList from '~/components/PostList';
 
-const Main = () => (
-  <Container>
-    <section>
-      <header>
-        <Title>Feeling grateful today?</Title>
-      </header>
-      <Content>
-        <div className="actions">
-          <button type="button">←</button>
-          <strong>24th set</strong>
-        </div>
-        <PostList />
-      </Content>
-      <footer>
-        <form>
-          <textarea
-            name=""
-            id=""
-            rows="5"
-            placeholder={'Type something you are grateful for and press "Enter" to share with everyone'}
-          />
-        </form>
-      </footer>
-    </section>
-  </Container>
-);
+function Main() {
+  return (
+    <Container>
+      <section>
+        <header>
+          <Title>Feeling grateful today?</Title>
+        </header>
+        <Content>
+          <div className="actions">
+            <button type="button">←</button>
+            <strong>24th set</strong>
+          </div>
+          <PostList />
+        </Content>
+        <footer>
+          <form>
+            <textarea
+              name=""
+              id=""
+              rows="5"
+              placeholder={
+                'Type something you are grateful for and press "Enter" to share with everyone'
+              }
+            />
+          </form>
+        </footer>
+      </section>
+    </Container>
+  );
+}
 
 export default Main;
