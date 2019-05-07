0ffdfb527acf98f6a22aee175d75d5c9fa90d0c8
Matheus Condini <matheus.condini@gmail.com>
2019-05-07 09:18:17 -0300

    create handle for textinput enter pressed and changed with react hooks

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container, Title, Content } from './styles';
 
 import PostList from '~/components/PostList';
 
 function Main() {
+  const [post, setPost] = useState('');
+
+  function handleEnter(event) {
+    if (event.keyCode === 13) {
+      console.log(post);
+
+      event.preventDefault();
+    }
+  }
+
+  function handleChange(event) {
+    setPost(event.target.value);
+  }
+
   return (
     <Container>
       <section>
@@ -19,16 +33,17 @@ function Main() {
           <PostList />
         </Content>
         <footer>
-          <form>
-            <textarea
-              name=""
-              id=""
-              rows="5"
-              placeholder={
-                'Type something you are grateful for and press "Enter" to share with everyone'
-              }
-            />
-          </form>
+          <textarea
+            name="post"
+            id="#post"
+            rows="5"
+            placeholder={
+              'Type something you are grateful for and press "Enter" to share with everyone'
+            }
+            value={post}
+            onKeyDown={handleEnter}
+            onChange={handleChange}
+          />
         </footer>
       </section>
     </Container>
