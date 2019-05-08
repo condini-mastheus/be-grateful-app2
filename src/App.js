9f26e6fadd48345c311c4542e690e097ef743f75
Matheus Condini <matheus.condini@gmail.com>
2019-05-08 09:10:04 -0300

    Configure redux store management

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+
 import GlobalStyles from '~/styles/global';
+
+import store from './store';
 
 import Brand from '~/components/Brand';
 import Main from '~/pages/Main';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyles />
       <div className="App">
         <Brand />
         <Main />
       </div>
-    </>
+    </Provider>
   );
 }
 
