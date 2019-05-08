e5c2235801d706428495ae44e4cc4daf49734f10
Matheus Condini <matheus.condini@gmail.com>
2019-05-08 10:04:24 -0300

    Configure reacttotron for log sagas request to api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './config/reactotron';
 import { Provider } from 'react-redux';
 
 import GlobalStyles from '~/styles/global';
--- src/store/index.js
+++ src/store/index.js
@@ -6,20 +6,18 @@ import sagas from './sagas';
 
 const middlewares = [];
 
-// const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
-const sagaMonitor = null;
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
 
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 middlewares.push(sagaMiddleware);
 
-// const composer = process.env.NODE_ENV === 'development'
-//   ? compose(
-//     applyMiddleware(...middlewares),
-//     console.tron.createEnhancer(),
-//   )
-//   : compose(applyMiddleware(...middlewares));
-const composer = compose(applyMiddleware(...middlewares));
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(
+    applyMiddleware(...middlewares),
+    console.tron.createEnhancer(),
+  )
+  : compose(applyMiddleware(...middlewares));
 
 const store = createStore(reducers, composer);
 
