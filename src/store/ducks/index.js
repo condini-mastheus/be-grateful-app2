1d3c70888a3217c391eae8de9be5f76f22a75dec
Matheus Condini <matheus.condini@gmail.com>
2019-05-08 10:07:30 -0300

    Fix reducer bad name

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
-import post from './posts';
+import posts from './posts';
 
 export default combineReducers({
-  post,
+  posts,
 });
--- src/store/ducks/posts.js
+++ src/store/ducks/posts.js
@@ -2,7 +2,11 @@ import Immutable from 'seamless-immutable';
 
 export const Types = {};
 
-const INITIAL_STATE = Immutable({});
+const INITIAL_STATE = Immutable({
+  isLoading: true,
+  isSending: false,
+  data: [],
+});
 
 export default function Posts(state = INITIAL_STATE, action) {
   switch (action.type) {
