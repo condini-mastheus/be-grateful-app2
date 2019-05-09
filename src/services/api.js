904b93b517fe8d28c91de763055bc1ea1639bbaa
condini-mastheus <matheus.condini@gmail.com>
2019-05-09 20:04:44 -0300

    Remove json-server and implement firebase

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : '',
+  baseURL: 'https://be-grateful-2.firebaseio.com/',
 });
 
 export default api;
