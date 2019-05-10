788e6fecb8fbe9dd5c07d5b063ba8d74818c0163
Matheus Condini <matheus.condini@gmail.com>
2019-05-10 09:52:36 -0300

    Return .eslint plugins and add condition on config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
-const { addBabelPlugin, override } = require('customize-cra');
+const { addBabelPlugin, override, disableEsLint } = require('customize-cra');
 
 module.exports = override(
+  process.env.NODE_ENV === 'development' ? disableEsLint() : null,
   addBabelPlugin([
     'root-import',
     {
