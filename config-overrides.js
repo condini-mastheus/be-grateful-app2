f78cefbee62a77daceb94b445aaba2adf3682c24
Matheus Condini <matheus.condini@gmail.com>
2019-05-10 10:33:29 -0300

    Fix config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,6 @@
-const { addBabelPlugin, override, disableEsLint } = require('customize-cra');
+const { addBabelPlugin, override } = require('customize-cra');
 
 module.exports = override(
-  process.env.NODE_ENV === 'development' ? disableEsLint() : null,
   addBabelPlugin([
     'root-import',
     {
